184d24eaeceab6e6983ea4ffc7e71d0295c26fcc
cgardn <christopher.gardn@gmail.com>
2020-04-23 15:47:23 -0400

    Updates

---
+++ precache-manifest.bbfb79a3ea089b44db1250f71380a33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a642f1eb12e2f6c6d64765d1194df03b",
+    "url": "/react-emporium/index.html"
+  },
+  {
+    "revision": "efa56102970c11f247c8",
+    "url": "/react-emporium/static/css/main.2a3b699c.chunk.css"
+  },
+  {
+    "revision": "b65ae15ef70714a54277",
+    "url": "/react-emporium/static/js/2.02b97adb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-emporium/static/js/2.02b97adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa56102970c11f247c8",
+    "url": "/react-emporium/static/js/main.21ad4cbd.chunk.js"
+  },
+  {
+    "revision": "4fb11e96e1b366e0fce4",
+    "url": "/react-emporium/static/js/runtime-main.c7682d1d.js"
+  }
+]);
